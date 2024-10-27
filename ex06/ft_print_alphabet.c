5415ee3b933def7dce0a7600a07a2bceec8b3bef
marinezh <mazhivotova@gmail.com>
2024-10-27 19:18:01 +0200

    create a repo for piscine reloaded ex

---
+++ ex06/ft_print_alphabet.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <unistd.h>
+
+
+void ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+void ft_print_alphabet(void)
+{
+    char c;
+
+    c = 'a';
+    while(c <= 'z')
+    {
+        ft_putchar(c);
+        c++;
+    }
+}
+
+int main()
+{
+  ft_print_alphabet();
+  printf("\n");
+  return 0;
+}
