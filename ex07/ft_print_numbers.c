202c03de5f4ce883f0fee11759af01ca37b74099
marinezh <mazhivotova@gmail.com>
2024-10-27 19:39:10 +0200

    add ex07

---
+++ ex07/ft_print_numbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+void ft_print_numbers(void)
+{
+    char c;
+
+    c = '1';
+    while(c <= '9')
+    {
+        ft_putchar(c);
+        c++;
+    }
+}
+
+int main()
+{
+    ft_print_numbers();
+    return 0;
+}
