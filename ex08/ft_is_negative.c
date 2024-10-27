4a18e9e0e03eb03060cc6dae2a930009bfacbce2
marinezh <mazhivotova@gmail.com>
2024-10-27 19:49:48 +0200

    add ex08

---
+++ ex08/ft_is_negative.c
@@ -0,0 +1,22 @@
+//#include <stdio.h>
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+void ft_is_negative(int n)
+{
+    if(n >= 0)
+        ft_putchar('P');
+    else
+        ft_putchar('N');
+    
+}
+
+int main()
+{
+    ft_is_negative(0);
+    return 0;
+}
